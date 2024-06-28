80b512d35c2484952b7bb74d564ec0fe21d79cda
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-28 08:10:17 +0530

    Expense Project Terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -34,6 +34,7 @@ vpc = {
     web_subnet_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
     app_subnet_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
     db_subnet_cidr  = ["10.0.4.0/24", "10.0.5.0/24"]
+    azs             = ["us-east-1a", "us-east-1b"]
   }
 }
 
--- main.tf
+++ main.tf
@@ -46,11 +46,12 @@
 module "vpc" {
   source = "git::https://github.com/raghudevopsb79/tf-module-vpc.git"
 
-  for_each         = var.vpc
-  vpc_cidr_block   = each.value["vpc_cidr_block"]
+  for_each        = var.vpc
+  vpc_cidr_block  = each.value["vpc_cidr_block"]
   web_subnet_cidr = each.value["web_subnet_cidr"]
   app_subnet_cidr = each.value["app_subnet_cidr"]
   db_subnet_cidr  = each.value["db_subnet_cidr"]
+  azs             = each.value["azs"]
 
   tags = var.tags
   env  = var.env
