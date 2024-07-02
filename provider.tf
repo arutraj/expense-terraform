89c32baf997627c6e3e7bceb783677af01b94591
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-02 09:11:02 +0530

    Expense Project Terraform

--- provider.tf
+++ provider.tf
@@ -1,14 +1,7 @@
 terraform {
   backend "s3" {}
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
 }
 
 provider "http" {}
 
-provider "kubectl" {}
 
