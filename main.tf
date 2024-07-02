a6f999732ea0c34b04de17e94c4b42a0c55c444d
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-02 07:47:13 +0530

    Expense Project Terraform

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ module "vpc" {
 
   for_each         = var.vpc
   vpc_cidr_block   = each.value["vpc_cidr_block"]
-  web_subnet_cidr  = each.value["web_subnet_cidr"]
-  app_subnet_cidr  = each.value["app_subnet_cidr"]
+  lb_subnet_cidr   = each.value["web_subnet_cidr"]
+  eks_subnet_cidr  = each.value["app_subnet_cidr"]
   db_subnet_cidr   = each.value["db_subnet_cidr"]
   azs              = each.value["azs"]
   default_vpc_id   = each.value["default_vpc_id"]
