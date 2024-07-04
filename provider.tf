222bcf21dbcfb658cd14f87b2d0dacfcabe0399e
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-04 08:42:23 +0530

    Expense Project Terraform

--- provider.tf
+++ provider.tf
@@ -4,4 +4,9 @@ terraform {
 
 provider "http" {}
 
+provider "vault" {
+  address         = "https://vault-internal.rdevopsb79.online:8200"
+  token           = var.vault_token
+  skip_tls_verify = true
+}
 
