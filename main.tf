d68c3d1ffb5df01ecd189ee8fedd2054a9eda2f5
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-05 08:23:25 +0530

    Expense Project Terraform

--- main.tf
+++ main.tf
@@ -76,12 +76,13 @@ module "eks" {
 module "rds" {
   source = "git::https://github.com/raghudevopsb79/tf-module-rds.git"
 
-  for_each       = var.rds
-  engine         = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  env            = var.env
-  family         = each.value["family"]
-  instance_class = each.value["instance_class"]
-  subnet_ids     = module.vpc["main"].db_subnet_ids
+  for_each        = var.rds
+  engine          = each.value["engine"]
+  engine_version  = each.value["engine_version"]
+  env             = var.env
+  family          = each.value["family"]
+  instance_class  = each.value["instance_class"]
+  subnet_ids      = module.vpc["main"].db_subnet_ids
+  eks_subnet_cidr = module.vpc["main"].eks_subnet_cidr
 }
 
